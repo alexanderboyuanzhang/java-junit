4aca6ba8ece58a1d657873d33ea1a7870f0246ff
bzhang111 <alexander.boyuan.zhang@gmail.com>
2019-03-30 17:55:15 +0100

    Junit

---
+++ JunitSuiteTest/Readme.md
@@ -0,0 +1,8 @@
+# JUnit Suite Test
+
+### Boyuan Zhang
+
+
+
+# Reference
+https://www.tutorialspoint.com/junit/junit_suite_test.htm
---
+++ JunitSuiteTest/src/com/boyuanzhang/suite/TestJunit1.java
@@ -0,0 +1,20 @@
+package com.boyuanzhang.suite;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Boyuan Zhang
+ *
+ */
+public class TestJunit1 {
+
+	String message = "Robert";
+	MessageUtil messageUtil = new MessageUtil(message);
+
+	@Test
+	public void testPrintMessage() {
+		System.out.println("Inside testPrintMessage()");
+		assertEquals(message, messageUtil.printMessage());
+	}
+}
